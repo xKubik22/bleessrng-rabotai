3418a5224e48dc29b3afc76e04f8eb8c452248e7
Дмитрий Бологов <bologov.d@mail.ru>
2022-05-07 17:34:34 +0700

    новая библиотека для чтения xlsx файлов

--- py/test2.py
+++ py/test2.py
@@ -1,7 +1,12 @@
 import xlsxwriter
+import openpyxl
 
-workbook = xlsxwriter.Workbook('ZP.xlsx')
-worksheet = workbook.add_worksheet()
-worksheet. write('A1', 'Hello world')
-workbook.close()
+wb_w = xlsxwriter.Workbook('ZP.xlsx')
+ws_w = wb_w.add_worksheet()
+ws_w. write('A1', 'Hello world')
+wb_w.close()
+
+wb_r = openpyxl.load_workbook('ZP.xlsx')
+print(wb_r.active.cell(1,1).value)
+
 
