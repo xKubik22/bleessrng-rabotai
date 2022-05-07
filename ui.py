13bd3db62f8fd21c8b8466489fe87a9d3e1342ad
Дмитрий Бологов <bologov.d@mail.ru>
2022-05-07 20:31:55 +0700

    рабочие чекбоксы

--- ui.py
+++ ui.py
@@ -14,18 +14,20 @@ class MainWindow(QtWidgets.QMainWindow):
         self.hide_widgets()
         self.ui.att_as_box.toggled.connect(self.show_as_widgets)
         self.ui.att_vp_box.toggled.connect(self.show_vp_widgets)
-        self.ui.att_vp_box.toggled.connect(self.show_vp_widgets)
-        self.ui.att_vp_box.toggled.connect(self.show_vp_widgets)
-        self.ui.att_vp_box.toggled.connect(self.show_vp_widgets)
+        self.ui.di_pemin_box.toggled.connect(self.show_di_widgets)
+        self.ui.di_nsd_box.toggled.connect(self.show_di_widgets)
 
     def hide_widgets(self):
         self.ui.att_as_count_box.hide()
-        self.ui.label.hide()
-        self.ui.label_2.hide()
-        self.ui.label_3.hide()
-        self.ui.pelena_count_box.hide()
-        self.ui.vibrik_count_box.hide()
-        self.ui.vsh_count_box.hide()
+        self.ui.frame_vp.hide()
+        self.ui.frame_di_pemin.hide()
+        self.ui.frame_di_nsd.hide()
+
+    def show_widgets(self):
+        self.ui.att_as_count_box.show()
+        self.ui.frame_vp.show()
+        self.ui.frame_di_pemin.show()
+        self.ui.frame_di_nsd.show()
 
     def disable_check_boxes(self):
         self.ui.att_vp_box.setEnabled(False)
@@ -47,6 +49,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def show_as_widgets(self):
         if self.ui.att_as_box.isChecked():
+            self.hide_widgets()
             self.ui.att_as_count_box.show()
             self.disable_check_boxes()
             self.ui.att_as_box.setEnabled(True)
@@ -54,23 +57,34 @@ class MainWindow(QtWidgets.QMainWindow):
             self.enable_check_boxes()
             self.ui.att_as_count_box.hide()
 
+    def show_di_widgets(self):
+        if self.ui.di_nsd_box.isChecked() or self.ui.di_pemin_box.isChecked():
+            self.disable_check_boxes()
+            self.ui.di_nsd_box.setEnabled(True)
+            self.ui.di_pemin_box.setEnabled(True)
+            self.ui.kp_box.setEnabled(True)
+        else:
+            self.enable_check_boxes()
+
+        if self.ui.di_nsd_box.isChecked():
+            self.ui.frame_di_nsd.show()
+        else:
+            self.ui.frame_di_nsd.hide()
+
+        if self.ui.di_pemin_box.isChecked():
+            self.ui.frame_di_pemin.show()
+        else:
+            self.ui.frame_di_pemin.hide()
+
     def show_vp_widgets(self):
         if self.ui.att_vp_box.isChecked():
             self.disable_check_boxes()
             self.ui.att_vp_box.setEnabled(True)
-            self.ui.label.show()
-            self.ui.label_2.show()
-            self.ui.label_3.show()
-            self.ui.pelena_count_box.show()
-            self.ui.vibrik_count_box.show()
-            self.ui.vsh_count_box.show()
+            self.hide_widgets()
+            self.ui.frame_vp.show()
         else:
-            self.ui.label.hide()
-            self.ui.label_2.hide()
-            self.ui.label_3.hide()
-            self.ui.pelena_count_box.hide()
-            self.ui.vibrik_count_box.hide()
-            self.ui.vsh_count_box.hide()
+            self.enable_check_boxes()
+            self.ui.frame_vp.hide()
 
 
 
