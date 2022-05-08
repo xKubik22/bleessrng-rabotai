4d41ee0e3a967ba37edefb4afe6ffea3d3b0668f
Дмитрий Бологов <bologov.d@mail.ru>
2022-05-08 14:03:43 +0700

    метод добавления объекта в таблицу

--- price_calculation.py
+++ price_calculation.py
@@ -1,24 +1,30 @@
 import xlsxwriter
 import openpyxl
-
-def write_object(object_name):
-	wb_r.active = 0
-	wb_w.active = 0
-	ws_w.write(wb_r.active.max_row,0,object_name)
+import os.path
 
 
-wb_w = xlsxwriter.Workbook('ZP.xlsx')
-ws_w = wb_w.add_worksheet()
-object_name_list = []
-wb_r = openpyxl.load_workbook('ZP.xlsx')
-print(wb_r.active.cell(1,1).value)
+def create_workbook(name: str):
+	book = openpyxl.Workbook()
+	book.save(name)
+	book.close()
 
-ws_r = wb_r.active
-for i, row in enumerate (ws_r.iter_rows()):
-	for cell in row:
-		object_name_list.append(wb_r.active.cell(wb_r.active.max_row,1).value)
-		write_object(object_name_list[i])
-		print(object_name_list)
-write_object("Zassssdasdagsddsa")
-wb_w.close()
+
+def write_object_in_sheet(object_name: str, book_name: str = 'ZP.xlsx'):
+	if not os.path.isfile(book_name):
+		create_workbook(book_name)
+	book = openpyxl.load_workbook(book_name)
+	sheet = book.active
+	sheet.append(tuple(object_name))
+	book.save(book_name)
+
+
+for i in range(5):
+	write_object_in_sheet(str(i))
+
+# wb_w = xlsxwriter.Workbook('ZP.xlsx')
+# ws_w = wb_w.add_worksheet()
+# object_name_list = []
+# wb_r = openpyxl.load_workbook('ZP.xlsx')
+
+
 
