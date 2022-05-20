2683658a4429a1b44716f6d9209d387cf37ef067
xKubik22 <50015567+xKubik22@users.noreply.github.com>
2022-05-20 22:01:32 +0700

    calculcate done2

--- price_calculation.py
+++ price_calculation.py
@@ -1,4 +1,3 @@
-import xlsxwriter
 import openpyxl
 from works_indexes import indexes 
 
@@ -13,29 +12,30 @@ def read_price_list():
 	price_list_done = {}
 	if 'Цены' in wb_r.sheetnames:
 		wb_r.active = wb_r['Цены']
-		for row in ws_r.iter_rows(min_row= 0, max_row=ws_r.max_row, max_col=ws_r.max_column, min_col=1):
+		for row in ws_r.iter_rows(min_row= 0, max_row=ws_r.max_row-1, max_col=ws_r.max_column, min_col=1):
 			for cell in row:
 				read_price_list.append(cell.value)	
 	else:
 		wb_r.create_sheet("Цены")
 	price_list_done = {read_price_list[i]: read_price_list[i+1] for i in range(0, len(read_price_list) - 1,2)}
+	price_list_done.pop(None)
+	wb_r.close('ZP.xlsx')
 	return price_list_done
 	
-def calculate_price():
-	object_work = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17']
+def calculate_price(object_work):
 	price_list = read_price_list()
 	print(price_list)
+	total_price = 0
 	for key in price_list.keys():
-		total_price += price_list[key] * object_work[key]
-	# print(total_price)
+		total_price += int(price_list[key]) * object_work[indexes[key]]
+	return total_price
 
-
-
-wb_r = openpyxl.load_workbook('ZP.xlsx')
-ws_r = wb_r.active
-read_price_list()
-calculate_price()
-wb_r.save('ZP.xlsx')
+if __name__ == "__main__":
+	wb_r = openpyxl.load_workbook('ZP.xlsx')
+	ws_r = wb_r.active
+	read_price_list()
+	calculate_price([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+	wb_r.save('ZP.xlsx')
 
 
 
