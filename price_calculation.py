8b5fc05ab28d2ee124eb36e35d0769781c534288
xKubik22 <50015567+xKubik22@users.noreply.github.com>
2022-05-20 21:27:10 +0700

    расчет цены почти финал_ОЧКА

--- price_calculation.py
+++ price_calculation.py
@@ -7,45 +7,66 @@ import openpyxl
 # 	print(wb_r.active)
 # 	ws_r.append(object_name_list)
 
-def calculate_total_price():
-	object_name_list = wb_r.sheetnames
-	price_list_done = read_price_list_done()
-	print(object_name_list)
-	
-	
-	if 'Цены' in object_name_list:
-		wb_r.active = 2
-		print(wb_r.active)
-		price_list  = [ws_r.cell(row=i,column=2).value for i in range(1,18)]
+def read_price_list():
+	read_price_list = []
+	price_list_done = {}
+	if 'Цены' in wb_r.sheetnames:
+		wb_r.active = wb_r['Цены']
+		for row in ws_r.iter_rows(min_row= 0, max_row=ws_r.max_row, max_col=ws_r.max_column, min_col=1):
+			for cell in row:
+				read_price_list.append(cell.value)	
 	else:
 		wb_r.create_sheet("Цены")
-	print(ws_r.max_column,ws_r.max_row)
-	print(price_list_done)
-	print(price_list,wb_r.active)
+	price_list_done = {read_price_list[i]: read_price_list[i+1] for i in range(0, len(read_price_list) - 1,2)}
+	return price_list_done
+	
+def calculate_price():
+	object_work = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17']
+	price_list = read_price_list()
+	print(price_list)
+	for key in price_list.keys():
+		total_price += price_list[key] * object_work[key]
+	# print(total_price)
 
-def read_price_list_done():
-	object_name_list = wb_r.sheetnames
-	price_list_done = []
-	if 'Расчет' in object_name_list:
-		wb_r.active = 1
-		work_column = ws_r.max_column
-		work_row = ws_r.max_row
-		for row in ws_r.iter_rows(min_row= work_row, max_row=work_row, max_col=work_column, min_col=2):
-			for cell in row:
-				price_list_done.append(cell.value)	
-	else:
-		wb_r.create_sheet("Расчет")
-	return 	price_list_done	
 
-	# for column in range(1,wb_s.max_column)
-	# total_cost
 
 wb_r = openpyxl.load_workbook('ZP.xlsx')
 ws_r = wb_r.active
-#write_object("Zassssdasdagsddsa")
-calculate_total_price()
+read_price_list()
+calculate_price()
 wb_r.save('ZP.xlsx')
 
 
 
 
+# def read_price_list():
+# 	object_name_list = wb_r.sheetnames
+# 	price_list_done = []
+# 	if 'Расчет' in object_name_list:
+# 		wb_r.active = wb_r['Расчет']
+# 		print(wb_r.active)
+# 		work_column = ws_r.max_column
+# 		work_row = ws_r.max_row
+# 		print(work_column,work_row)
+# 		for row in ws_r.iter_rows(min_row= work_row, max_row=work_row, max_col=work_column, min_col=2):
+# 			for cell in row:
+# 				price_list_done.append(cell.value)	
+# 	else:
+# 		wb_r.create_sheet("Расчет")
+# 	print(price_list_done)
+	 		
+
+	# for column in range(1,wb_s.max_column)
+	# total_cost
+ # if 'Расчет' in object_name_list:
+	# 	wb_r.active = 1
+	# 	print(wb_r.active)
+	# 	work_column = ws_r.max_column
+	# 	work_row = ws_r.max_row
+	# 	for row in ws_r.iter_rows(min_row= work_row, max_row=work_row, max_col=work_column, min_col=2):
+	# 		for cell in row:
+	# 			price_list_done.append(cell.value)			
+	# else:
+	# 	wb_r.create_sheet("Расчет")
+	
+	# print(price_list_done,wb_r.active)
